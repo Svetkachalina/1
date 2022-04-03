fd4e31d3250108776455fca3490b9d3363e4823d
Svetlana Murdoch <66595456+Svetkachalina@users.noreply.github.com>
2022-04-03 16:53:03 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
+  // добавим таймер
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+    // таймер выше  
+  pagination: {
       el: ".swiper-pagination",
       type: "progressbar",
     },
@@ -8,3 +13,38 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+// slider 2 services
+{
+  var swiper = new Swiper(".swiper_services", {
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  spaceBetween: 314,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+}
+
+
+
+  // автоматически
+  // {
+  //   var swiper = new Swiper(".mySwiper", {
+  //     spaceBetween: 30,
+  //     centeredSlides: true,
+  //     autoplay: {
+  //       delay: 2500,
+  //       disableOnInteraction: false,
+  //     },
+  //     pagination: {
+  //       el: ".swiper-pagination",
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: ".swiper-button-next",
+  //       prevEl: ".swiper-button-prev",
+  //     },
+  //   });
+  // }
